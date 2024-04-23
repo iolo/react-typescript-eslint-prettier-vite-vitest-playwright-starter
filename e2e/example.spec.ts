e5e1e86b71a72b322a07c0c5d97bd6505a1b8d7b
Dongsu Jang <77693+iolo@users.noreply.github.com>
2024-04-24 00:35:16 +0900

    npm install playwright

---
+++ e2e/example.spec.ts
@@ -0,0 +1,11 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'count is' }).click();
+
+  const button = page.getByRole('button', { name: 'count is' })
+
+  await expect(button).toContainText("count is 1")
+});
